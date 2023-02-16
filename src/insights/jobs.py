fe238fbf6335c2ae048f25a293935993b6fea758
Mariana <marinhomariana8@gmail.com>
2023-02-16 16:03:06 -0300

    :sparkles: :feat: add read function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,16 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path) as file:
+        reader = csv.reader(file, delimiter=",")
+        data = []
+        for row in reader:
+            data.append(row)
+        return data
 
 
 def get_unique_job_types(path: str) -> List[str]:
