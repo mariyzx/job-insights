1ca55224813193b2dcca89666f45785a30a182e5
Mariana <marinhomariana8@gmail.com>
2023-02-16 17:09:34 -0300

    :sparkles: :feat: add get_max_salary function

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,23 @@
 from typing import Union, List, Dict
+import csv
+
+
+def read(path: str) -> List[Dict]:
+    with open(path) as file:
+        reader = csv.DictReader(file, delimiter=",")
+        data = [row for row in reader]
+
+        return data
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    all_salaries = []
+    for job in jobs:
+        if job["max_salary"] != '' and job["max_salary"].isnumeric():
+            all_salaries.append(int(job["max_salary"]))
+    max_salary = max(all_salaries)
+    return max_salary
 
 
 def get_min_salary(path: str) -> int:
