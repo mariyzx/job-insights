9d1c0a2380cb7fc869c236df7a1817e615048c22
Mariana <marinhomariana8@gmail.com>
2023-02-16 16:56:50 -0300

    :recycle: :refactor: changing variable name

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -13,8 +13,8 @@ def read(path: str) -> List[Dict]:
 def get_unique_industries(path: str) -> List[str]:
     industries = read(path)
     unique_industries = {industry["industry"] for industry in industries}
-    withoutEmptyIndustry = list(filter(None, unique_industries))
-    return withoutEmptyIndustry
+    without_empty_industry = list(filter(None, unique_industries))
+    return without_empty_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
